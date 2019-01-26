5cc46303f82c2bfab112d4cc85973401c16fc3b0
Ofek Censor <ofek.censor@gmail.com>
2019-01-26 21:57:09 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Technion Math XBlock
+====================
+
+An edX XBlock that uses WeBWorK's PG as its backend.
---
+++ setup.py
@@ -0,0 +1,41 @@
+"""Setup for math XBlock."""
+
+import os
+
+from setuptools import setup
+
+
+def package_data(pkg, roots):
+    """Generic function to find package_data.
+
+    All of the files under each of the `roots` will be declared as package
+    data for package `pkg`.
+
+    """
+    data = []
+    for root in roots:
+        for dirname, _, files in os.walk(os.path.join(pkg, root)):
+            for fname in files:
+                data.append(os.path.relpath(os.path.join(dirname, fname), pkg))
+
+    return {pkg: data}
+
+
+setup(
+    name='math-xblock',
+    version='0.1',
+    description='math XBlock',   # TODO: write a better description.
+    license='UNKNOWN',          # TODO: choose a license: 'AGPL v3' and 'Apache 2.0' are popular.
+    packages=[
+        'math',
+    ],
+    install_requires=[
+        'XBlock',
+    ],
+    entry_points={
+        'xblock.v1': [
+            'math = math:MathXBlock',
+        ]
+    },
+    package_data=package_data("math", ["static", "public"]),
+)
